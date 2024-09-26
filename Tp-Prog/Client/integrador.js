225215f0d3386fd20e7c94975f70c705a03eabea
santichieu <santi.chieu@gmail.com>
2024-09-26 19:06:30 -0300

    js

--- Tp-Prog/Client/integrador.js
+++ Tp-Prog/Client/integrador.js
@@ -1,3 +1,31 @@
+
+
+/*
+
 fetch("https://dolarapi.com/v1/dolares")
   .then(response => response.json())
-  .then(data => {console.log(data[5].venta); console.log (data[4].venta)});/*document.write(data[5].venta); document.write(data[4].venta)});*/
+  .then(data => {console.log(data[5].venta); console.log (data[4].venta)});
+
+
+  fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => {document.getElementsByClassName("precio-venta")[0].innerHTML= data[5].venta});
+
+
+
+ */
+
+fetch("https://dolarapi.com/v1/cotizaciones")
+  .then(response => response.json())
+  .then(data => {for (i=0; i<data.length; i++){
+    agregarCotizacion(data[i].nombre, data[i].venta, data[i].compra)}});
+
+  function agregarCotizacion(nombre, venta, compra){
+    var x = document.getElementsByClassName("dolar-oficial")[0].cloneNode(true)
+
+    x.querySelector(".dolar").innerHTML= nombre
+    x.querySelector(".precio-venta").innerHTML= venta
+    x.querySelector(".precio-compra").innerHTML= compra
+    document.body.appendChild(x)
+  }
+
