00ee5d728db322fa5ec83bcd93866f325c0ef899
Quimey Alejo <96157348+QuimeyAlejo@users.noreply.github.com>
2024-09-26 19:08:12 -0300

    prueba 1

--- .history/Tp-Prog/Client/integrador_20240926185721.js
+++ .history/Tp-Prog/Client/integrador_20240926185721.js
@@ -7,6 +7,4 @@ fetch("https://dolarapi.com/v1/dolares")
     fetch("https://dolarapi.com/v1/dolares/blue")
   .then(response => response.json())
   .then(data => console.log(data));
-  };
-
-  dolarBlue();
+  }
