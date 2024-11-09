364d5c4e015fd8526f367505d10d1141d443bcb8
Quimey Alejo <96157348+QuimeyAlejo@users.noreply.github.com>
2024-11-08 23:20:42 -0300

    server.py modificado y script js
    Modifique en la parte de server.py para que se pueda iterar y guardar en una lista o diccionario los datos de la api dolar y pedir desde localhost en el script de js

--- Tp-Prog/Server/server.py
+++ Tp-Prog/Server/server.py
@@ -1,21 +1,35 @@
 
 from flask import Flask, jsonify
+from flask_cors import CORS 
 import requests
 
-app = Flask(__name__)
-
+app = Flask(__name__) #esta  linea sirve para crear una app de flask e indicar __name__ a flask donde se encuenta el archivo principal de nuestro sv corte un main
+CORS(app)
 @app.route('/dolar', methods=['GET'])
 def get_api_data():  
     api_url = "https://dolarapi.com/v1/dolares"  
     response = requests.get(api_url)   
     print(response.json(), "Que traigo")  
-    if response.status_code == 200:
-        return jsonify(response.json())
+    if response.status_code == 200: #el cod 200 es cuando todo esta OK 
+        data = response.json()
+        info_moneda = [];
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'nombre': i['nombre'],
+                'moneda': i['moneda'],
+                'fechaActualizacion': i['fechaActualizacion']
+                
+            })
+        return jsonify(info_moneda)
     else:
-        return jsonify({'error': 'No se pudo encontrar algo...'}), response.status_code
+        return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
 
 
-@app.route('/cotizaciones', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_api_cotizaciones():
     api_url = "https://dolarapi.com/v1/cotizaciones"
     response = requests.get(api_url)
@@ -25,8 +39,8 @@ def get_api_cotizaciones():
     else:
         return jsonify({'error': 'No se pudo encontrar algo...'}), response.status_code
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': #la aplicacion se ejecute solo cuando el archivo se ejecuta directamente, no cuando se importa.
+    app.run(debug=True) # el modo debug en true sirve para que flask te tire los errores detallados si salta algo mal y vuelve a cargar cuando hay cambios en el codigo
 
 
            
