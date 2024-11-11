c9cbd5843def1c1fc3628f762e1598daf16123e2
Olivia Luciano <qalopez2000@gmail.com>
2024-11-11 19:52:16 -0300

    update py
    ---

--- Tp-Prog/Server/server.py
+++ Tp-Prog/Server/server.py
@@ -52,6 +52,8 @@ def get_cotizaciones():
     else:
         return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
 
+    
+
 if __name__ == '__main__': #la aplicacion se ejecute solo cuando el archivo se ejecuta directamente, no cuando se importa.
     app.run(debug=True) # el modo debug en true sirve para que flask te tire los errores detallados si salta algo mal y vuelve a cargar cuando hay cambios en el codigo
 
