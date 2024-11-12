4e905cb1c85eaee8ebec9887c220b6e1524d12d3
santichieu <santi.chieu@gmail.com>
2024-11-11 21:23:47 -0300

    py,contacto html, css
    funcion historico py, agregado mensaje en contacto html y estilos varios

--- Tp-Prog/Server/server.py
+++ Tp-Prog/Server/server.py
@@ -52,7 +52,27 @@ def get_cotizaciones():
     else:
         return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
 
+@app.route('/historico', methods=['GET'])
+def get_historico_data():  
+    api_url = "https://api.argentinadatos.com/v1/cotizaciones/dolares/"  
+    response = requests.get(api_url)   
+    print("GET HISTORICO")  
+    if response.status_code == 200: #el cod 200 es cuando todo esta OK 
+        data = response.json()
+        info_moneda = [];
+        
+        for i in data:
+            info_moneda.append({
+                'casa': i['casa'],
+                'compra': i['compra'],
+                'venta': i['venta'],
+                'fecha': i['fecha']   
+            })
+        return jsonify(info_moneda)
+    else:
+        return jsonify({'error': 'Paren la rotativa, hay un error...'}), response.status_code
     
+
 
 if __name__ == '__main__': #la aplicacion se ejecute solo cuando el archivo se ejecuta directamente, no cuando se importa.
     app.run(debug=True) # el modo debug en true sirve para que flask te tire los errores detallados si salta algo mal y vuelve a cargar cuando hay cambios en el codigo
